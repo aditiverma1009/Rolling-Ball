6cf5019b69cc0d9cb5ac9477afc618068d3af54b
aditiverma1009 <aditiverma1009@gmail.com>
2018-09-27 09:51:15 +0530

    Base game

---
+++ Assets/Script/PlayerComp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerComp : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+    public Text winText;
+    public Text countText;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        setCountText();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveVertical, 0.0f, -moveHorizontal);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up")){
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText(){
+        countText.text = "Score: "+count.ToString();
+        if(count>=5){
+            winText.text = "You Win!";
+        }
+    }
+}
